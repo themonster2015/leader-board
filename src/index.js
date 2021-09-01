f72288e5671b9568a929e0a8d01895959763a394
themonster2015 <quynhyen.vothi@gmail.com>
2021-09-01 21:49:40 +0200

    fixed post score behavior on form submit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import fillData from './fillData';
 
 const refresh = document.getElementById('refreshScores');
 const submitScore = document.querySelector('form');
-const name = document.getElementById('name');
+const user = document.getElementById('name');
 const score = document.getElementById('score');
 
 refresh.addEventListener('click', async () => {
   fillData(await fetchScores());
 });
 
-submitScore[0].addEventListener('click', (e) => {
+submitScore.addEventListener('submit', (e) => {
   e.preventDefault();
-  postScore(name.value, score.value);
-  name.value = '';
+  postScore(user.value, Number(score.value));
+  user.value = '';
   score.value = '';
 });
+
+window.onload = async () => fillData(await fetchScores());
--- src/postScore.js
+++ src/postScore.js
@@ -1,6 +1,6 @@
 const postScore = (user, score) => {
   const data = { user, score };
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WyWIaxHGGQkxp04ftWLN/scores',
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WyWIaxHGGQkxp04ftWLN/scores/',
     {
       method: 'POST',
       headers: {
